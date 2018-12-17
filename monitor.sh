f5f38cb7b472d3312ac8256eaa3382271cce17b2
cascafico <cascafico@gmail.com>
2018-12-17 11:09:17 +0100

    past days funcionality added

--- monitor.sh
+++ monitor.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
-# first argument admin boundary area name
-# second argument how many days ago
 
 if [ $# -eq 0 ]
    then
-     echo "first argument area name"
-     echo "second argument (optional) how many days ago monitor changes"
+     echo "first argument how many days ago monitor changes"
+     echo "second argument area name (double quoted if multistring)"
      echo "...exiting"
      exit
 fi
 
-T0=`date -d "yesterday 00:00" '+%Y-%m-%d'`T00:00:00Z
-T1=`date +"%Y-%m-%d"`T00:00:00Z
-IERI=`date -d "yesterday 00:00" '+%Y-%m-%d'`
-OGGI=`date +"%Y-%m-%d"`
-if [[ "$2" =~ ^[0-9]+$ ]] ; then 
-   T0=`date -d "$2 days ago 00:00" '+%Y-%m-%d'`T00:00:00Z
-   IERI=`date -d "$2 days ago 00:00" '+%Y-%m-%d'`
-fi
+if [ -z "$2" ]
+  then
+    echo "No area supplied"
+    exit
+fi    
+PLACE=$2
+PLACE=${PLACE// /%20}
 
 
 RUN=`date`
+T0=`date -d "yesterday 00:00" '+%Y-%m-%d'`T00:00:00Z
+T1=`date +"%Y-%m-%d"`T00:00:00Z
 
-AREACODE=`curl -s "http://overpass-api.de/api/interpreter?data=area%5B%22boundary%22%3D%22administrative%22%5D%5B%22name%22%3D%22$1%22%5D%3Bout%20ids%3B%0A" | grep 3600 | awk -F "\"" '{print $2}'`
+if [[ "$1" =~ ^[0-9]+$ ]] ; then 
+   T0=`date -d "$1 days ago 00:00" '+%Y-%m-%d'`T00:00:00Z
+   else echo "first argument must be an integer"
+   exit
+fi
 
+AREACODE=`curl -s "http://overpass-api.de/api/interpreter?data=area%5B%22boundary%22%3D%22administrative%22%5D%5B%22name%22%3D%22$PLACE%22%5D%3Bout%20ids%3B%0A" | grep 3600 | awk -F "\"" '{print $2}'`
+echo $PLACE
+echo $AREACODE
 if [ -z $AREACODE  ]
    then
      echo "area name not found (please case sensitive)"
@@ -39,13 +45,13 @@ echo "extracting CAIFVG yesterday differences ..."
 
 wget -O adiff$OGGI.xml "$QUERY"
 
-cat adiff$OGGI.xml | grep "$IERI\|$OGGI" | grep changeset | awk ' { print substr($0,index($0, "changeset")+11,8) }' > changeset.lst
+cat adiff$OGGI.xml | grep changeset | awk -F "changeset=" ' { print $2 }'| awk -F "\"" ' { print $2 }' > changeset.lst
 
 echo "sorting and compacting changeset list"
 sort -u changeset.lst -o changeset.lst
 CHAN=`cat changeset.lst | wc -l`
 
-echo "<h3>Changeset(s) created in interval</h3><br> Query: operator=CAI or operator=Club Alpino Italiano <br> Area: $1<br>Interval: since $2 days ago<p>" > index.html
+echo "<h3>Changeset(s) created in interval</h3><br> Query: operator=CAI or operator=Club Alpino Italiano <br> Area: $PLACE<br>Interval: since $1 days ago<p>" > index.html
 echo "<style>table, th, td { border: 1px solid black; border-collapse: collapse; }</style>" >> index.html
 echo "<table><tr><th>OSMcha</th><th>Achavi</th></tr>" >> index.html
 
